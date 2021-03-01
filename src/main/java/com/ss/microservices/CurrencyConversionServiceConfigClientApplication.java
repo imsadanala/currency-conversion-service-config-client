b3aa4a9d29190a957c5d14011517dffe3aaba849
imsadanala <ssadanala@deloitte.com>
2021-03-01 22:58:10 +0530

    currency conversion basic structure changes

---
+++ src/main/java/com/ss/microservices/CurrencyConversionServiceConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.ss.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyConversionServiceConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceConfigClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/microservices/CurrencyConversionServiceConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
