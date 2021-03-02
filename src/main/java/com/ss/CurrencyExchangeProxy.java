f59d336c4f22565019a804bdac9ddbcf7f90cded
imsadanala <ssadanala@deloitte.com>
2021-03-02 21:39:03 +0530

    Fiegn Client Side LoadBalancing

--- src/main/java/com/ss/CurrencyExchangeProxy.java
+++ src/main/java/com/ss/CurrencyExchangeProxy.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.ss.dto.CurrencyConversion;
 
-
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
 	@GetMapping(value = "currency-exchange/from/{from}/to/{to}")
